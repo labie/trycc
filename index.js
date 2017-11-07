c5a7ed966252eb82eebd10ff7bc5ecf2c34a29b4
Andy Zhang <zch.ustc@gmail.com>
2017-11-07 15:44:53 +0800

    add circleci config and test case

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const express = require('express')
-const app = express()
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+var express = require('express');
+var app = express();
+app.get('/', function (req, res) {
+  res.status(200).send('ok');
+});
+var server = app.listen(3000, function () {
+  var port = server.address().port;
+  console.log('Example app listening at port %s', port);
+});
+module.exports = server;
